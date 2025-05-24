3bcad007f557fdbdfa3d1eb9e7750be66d794f2c
Raymundo Mendoza <noisygott@gmail.com>
2025-05-24 15:29:21 -0600

    rutas con nombre implementadas

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class ButtonsScreen extends StatelessWidget {
+  static const String name = 'buttons_screen';
   const ButtonsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Buttons Screen'),
-      ),
+      appBar: AppBar(title: const Text('Buttons Screen')),
       body: const Placeholder(),
     );
   }
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class CardsScreen extends StatelessWidget {
+  static const String name = 'cards_screen';
   const CardsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cards Screen'),
-      ),
+      appBar: AppBar(title: const Text('Cards Screen')),
       body: const Placeholder(),
     );
   }
