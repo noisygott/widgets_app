0ac837081b262cae1644c93764d16b4049d47989
Raymundo Mendoza <noisygott@gmail.com>
2025-05-24 17:40:28 -0600

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+  static const String name = 'animate_screen';
+  const AnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
